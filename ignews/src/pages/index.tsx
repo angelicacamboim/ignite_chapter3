2feb51252cf2e52c78521cda7435d577bca6e41f
Angelica Bayer <angelicacamboim@gmail.com>
2021-11-23 18:25:39 -0300

    styles

---
+++ ignews/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head' //title dinamico
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>ig.news</title>
+			</Head>
+			<h1>Ola1</h1>
+		</>
+	)
+}
