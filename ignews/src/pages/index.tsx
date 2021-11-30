f8d07dbc7d8a613bd901466f35300f43245750ce
Angelica Bayer <angelicacamboim@gmail.com>
2021-11-30 19:23:56 -0300

    stripe and authentication

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,12 +1,26 @@
 import Head from 'next/head' //title dinamico
 import { SubscribeButton } from '../components/SubscribeButton'
 import styles from './home.module.scss'
+import { GetStaticProps } from 'next'
+import { stripe } from '../components/services/stripe'
 
-export default function Home() {
+//chamadas api e gerar a pagina
+//client-side (depois da pagina ter carregado)
+//server-side (dinamico em tempo real)
+//static site generation
+
+interface HomeProps {
+	product: {
+		priceId: string
+		amount: number
+	}
+}
+
+export default function Home({ product }: HomeProps) {
 	return (
 		<>
 			<Head>
-				<title>ig.news</title>
+				<title>Home | ig.news</title>
 			</Head>
 			<main className={styles.contentContainer}>
 				<section className={styles.hero}>
@@ -16,13 +30,29 @@ export default function Home() {
 					</h1>
 					<p>
 						Get access to all the publications <br />
-						<span>for $9.90 month</span>
+						<span>for {product.amount} month</span>
 					</p>
-					<SubscribeButton />
+					<SubscribeButton priceId={product.priceId} />
 				</section>
 
 				<img src="/images/avatar.svg" alt="Girl coding" />
 			</main>
 		</>
 	)
+}
+
+//stripe SSG - buscar da api o preço (valor estático)
+export const getStaticProps: GetStaticProps = async () => {
+	const price = await stripe.prices.retrieve('price_1JzSdvH901wNIzOlhVgoJWtC', {
+		expand: ['product']
+	})
+
+	const product = {
+		priceId: price.id,
+		amount: new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: 'USD'
+		}).format(price.unit_amount / 100)
+	}
+	return { props: { product }, revalidate: 60 * 60 * 24 } //24 horas
 }
