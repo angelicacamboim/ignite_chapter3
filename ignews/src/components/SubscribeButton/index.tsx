ce8155a163310e12201b07d667687569cbde9f8d
Angelica Bayer <angelicacamboim@gmail.com>
2021-12-14 20:20:05 -0300

    prismic

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from 'next-auth/client'
+import { useRouter } from 'next/router'
 import { api } from '../../services/api'
 import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
@@ -11,12 +12,22 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
 	//verifica se o user está logado
 	const [session] = useSession()
 
+	const router = useRouter()
+
 	async function handleSubscribe() {
+		//se ele nao estiver logado no github
 		if (!session) {
 			signIn('github')
 			return
 		}
 
+		//se ele tiver uma sessão ativa no stripe/faundadb
+		if (session.activeSubscription) {
+			router.push('/posts')
+			return
+		}
+
+		//se ele estiver logado, mas nao possui uma sessão ativa no stripe
 		try {
 			const response = await api.post('/subscribe')
 
--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -15,6 +15,37 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async session(session){
+    try{
+      const userActiveSubscription = await fauna.query(
+        q.Get(
+          q.Intersection([
+            q.Match( //select subscription_by_user_ref
+              q.Index('subscription_by_user_ref'),
+              q.Select(
+                "ref",
+                q.Get(
+                  q.Match( //where user_by_email = session.user.email
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email)
+                  )
+                )
+              )
+            ),
+            q.Match(
+              q.Index('subscription_by_status'),
+              "active"
+            )
+             ] )
+        )
+      )
+
+      return {...session, activeSubscription: userActiveSubscription}
+      
+    }catch{
+      return {...session, activeSubscription: null}
+    }
+    },
     //signIn with github
     async signIn(user, account, profile) {
       const { email } = user
